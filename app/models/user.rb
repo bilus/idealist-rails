1da22b4ae6c428a5c442b8de2f1b4b7a2e9de79d
Martin Bilski <martinb@criticue.com>
2013-08-19 00:38:02 +0200

    ...

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :name
   # attr_accessible :title, :body
+
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
