edfebc18f65de28299916427ae6a3b0ef0684faa
Martin Bilski <martinb@criticue.com>
2013-08-18 23:49:48 +0200

    Login using Facebook (untested).

--- Gemfile
+++ Gemfile
@@ -34,7 +34,8 @@ gem 'fog'
 # gem 'sidekiq'
 
 gem 'devise'
-
+gem 'omniauth'
+gem 'omniauth-facebook'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,5 @@
-﻿# Use this hook to configure devise mailer, warden hooks and so forth.
+﻿require "omniauth-facebook"
+
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # ==> Mailer Configuration
@@ -243,4 +244,6 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
+  
+  config.omniauth :facebook, "122990291240131", "348aea9075e9a7286b2400136141ad4f", :scope => "email", :display => "popup"
 end
